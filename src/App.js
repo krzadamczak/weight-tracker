f9203afe0b50f17ff0af0b8da73b5e30a2d42fc3
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-06-22 22:50:44 +0200

    Added chart, minor changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BmiForm } from "./features/BmiForm/BmiForm";
+import Home from "./pages/Home";
 
 function App() {
     return (
-        <div className='App'>
+        <div className='app'>
             <nav>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/bmi-calculator'>Calculate your BMI!</NavLink>
             </nav>
             <Routes>
-                <Route path='/bmi-calculator' element={<BmiForm />}></Route>
+                <Route path='/' element={<Home />} />
+                <Route path='/bmi-calculator' element={<BmiForm />} />
                 {/* <BmiForm /> */}
             </Routes>
         </div>
